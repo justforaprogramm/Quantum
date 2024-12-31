a9af0ccc0cf3eeb3d70b8ee9a90b8f1d4d6c163d
justforaprogramm <benjamin.jugendfeuerwehr@gmail.com>
2024-12-31 01:18:27 +0100

    pprint

--- venv/source/hello.py
+++ venv/source/hello.py
@@ -1,25 +1,20 @@
-import cirq
-import qsimcirq
+import cirq, qsimcirq
+from pprint import pprint
 
 # Define qubits and a short circuit.
 q0, q1 = cirq.LineQubit.range(2)
 circuit = cirq.Circuit(cirq.H(q0), cirq.CX(q0, q1))
-print("Circuit:")
-print(circuit)
-print()
 
 # Simulate the circuit with Cirq and return the full state vector.
-print('Cirq results:')
 cirq_simulator = cirq.Simulator()
 cirq_results = cirq_simulator.simulate(circuit)
-print(cirq_results)
 
 # Simulate the circuit with qsim and return the full state vector.
-print('qsim results:')
 qsim_simulator = qsimcirq.QSimSimulator()
 qsim_results = qsim_simulator.simulate(circuit)
-print(qsim_results)
 
 samples = cirq.sample_state_vector(
     qsim_results.state_vector(), indices=[0, 1], repetitions=10)
-print(samples)
+
+print(f'Circuit:\n{circuit}', f'Cirq results:{cirq_results}', f'qsim results:{qsim_results}', 'samples:', sep='\n\n')
+pprint(samples)
